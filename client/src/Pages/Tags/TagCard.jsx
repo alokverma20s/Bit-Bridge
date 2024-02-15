c3ed45f9b7c6cf699664c46b05434af6d2171804
Gaurav <gauravghidode@gmail.com>
2024-02-15 12:26:33 +0530

    .

--- client/src/Pages/Tags/TagCard.jsx
+++ client/src/Pages/Tags/TagCard.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import { addDesc } from '../../services/operations/tagAPI';
+import axios from 'axios';
 
 const TagCard = ({ tag }) => {
     const dispatch = useDispatch();
@@ -11,15 +12,31 @@ const TagCard = ({ tag }) => {
     const [editFlag, setEditFlag] = useState(false);
     const [description, setDescription] = useState(tag.description);
     const [tempDesc, setTempDesc] = useState('');
+    const [wikiDesc, setWikiDesc] = useState('');
     const User = useSelector((state) => (state.currentUserReducer))
     function handleClick() {
         setEditFlag(!editFlag);
+        // getWikiDesc();
     }
     function handleSubmit(e) {
         e.preventDefault();
         dispatch(addDesc(description, tag?._id, setTempDesc));
         setEditFlag(!editFlag);
     }
+    async function getWikiDesc(){
+        try{
+            const url = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exlimit=1&titles=${tag?.tagName}&explaintext=1&exsectionformat=plain`
+            const a = await axios.get(url);
+            const b = a?.query?.pages;
+            setWikiDesc([Object.keys(b)[0]].extract);
+            console.log(wikiDesc);
+        }
+        
+        catch(e){
+            console.log(e);
+        }
+    }
+    
     return (
         <div className='tag'>
             <div className='tags-header'>
@@ -28,15 +45,18 @@ const TagCard = ({ tag }) => {
                     User?.result?.role === 'admin' && <span onClick={handleClick} className='add-desc-btn'><MdModeEdit /></span>
                 }
             </div>
+            <div id='tag-content'>
             {
                 editFlag ?
                     <form action="" onSubmit={handleSubmit}>
-                        <textarea type="text" rows='3' cols='10' onChange={(e)=>{setDescription(e.target.value)}}/>
+                        <textarea type="text" rows='3' cols='10' value={description} onChange={(e)=>{setDescription(e.target.value)}}/>
                         <button type='submit' className='inner-grad-btn' style={{ padding: "4px 10px", margin: "2px" }}>Add description</button>
                     </form>
                     :
                     <p>{tempDesc === ''? tag?.tagDescription: tempDesc}</p>
             }
+            </div>
+            
 
         </div>
     )
