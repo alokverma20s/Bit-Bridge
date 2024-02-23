45d5957b1559d9309e3dbe366aa05565aa38a238
Gaurav <gauravghidode@gmail.com>
2024-02-23 15:44:52 +0530

    enhancing answer voting

--- client/src/Pages/Questions/DisplayAnswer.jsx
+++ client/src/Pages/Questions/DisplayAnswer.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Avatar from '../../components/Avatar/Avatar'
 import { Link, useLocation, useParams } from 'react-router-dom'
@@ -9,7 +10,7 @@ import { FaUserTie } from "react-icons/fa6";
 import { toast } from 'react-hot-toast'
 import { BiSolidUpvote } from "react-icons/bi";
 import { BiSolidDownvote } from "react-icons/bi";
-
+import {BiUpvote, BiDownvote} from "react-icons/bi";
 import { deleteAnswer } from '../../actions/question.js'
 import { voteAnswer } from '../../actions/question.js'
 
@@ -23,31 +24,86 @@ const DisplayAns = ({ question }) => {
     dispatch(deleteAnswer(id, answerId, noOfAnswers - 1));
   }
 
+  const [currentVotes, setCurrentVotes] = useState(Array(question.answer.length).fill(0));
+  const [currentUpvote, setCurrentUpvote] = useState(Array(question.answer.length).fill(0));
+  const [currentDownvote, setCurrentDownvote] = useState(Array(question.answer.length).fill(0));
 
-  const handleUpVote = (answerId) => {
+  
+  useEffect(()=>{
+    question.answer.map((ans, index)=>{
+      currentVotes[index]=(ans.upVote.length - ans.downVote.length);
+      console.log(ans.upVote);
+      console.log(User.result._id);
+      if(ans.upVote.includes(User.result._id)){
+        currentUpvote[index]=1;
+      }
+      if(ans.downVote.includes(User.result._id)){
+        currentDownvote[index]=1;
+      }
+    })
+  });
+
+  console.log(currentVotes);
+
+  const handleUpVote = (answerId, index) => {
     if(User==null){
       toast("Login to vote");
     }
-    else
-      dispatch(voteAnswer(id, answerId, 'upVote', User.result?._id))
+    else {
+      dispatch(voteAnswer(id, answerId, 'upVote', User.result?._id));
+      if(currentDownvote[index]==1){
+        currentDownvote[index]=0;
+        currentUpvote[index]=1;
+        currentVotes[index]=currentVotes[index]+2;
+      }
+      else if(currentUpvote[index]==0){
+        currentVotes[index]++;
+        currentUpvote[index]=1;
+      }
+      else{
+        currentUpvote[index]=0;
+        currentVotes[index]--;
+      }
+    }
+      
   }
-  const handleDownVote = (answerId) => {
+  const handleDownVote = (answerId, index, downVoteArray) => {
     if(User==null){
       toast("Login to vote");
     }
-    else
+    else{
       dispatch(voteAnswer(id, answerId, 'downVote', User.result?._id))
+      if(currentUpvote[index]==1){
+        currentUpvote[index]=0;
+        currentDownvote[index]=1;
+        currentVotes[index]=currentVotes[index]-2;
+      }
+      else if(currentDownvote[index]==0){
+        currentVotes[index]--;
+        currentDownvote[index]=1;
+      }
+      else{
+        currentDownvote[index]=0;
+        currentVotes[index]++;
+      }
+    }
+      
   }
   // console.log(question.answer);
   return (
     <div>
       {
-        question.answer.map((ans) => (
+        question.answer.map((ans, index) => (
           <div className="ans-outer-container">
             <div className='ans-voting'>
-              <BiSolidUpvote onClick={()=>handleUpVote(ans._id)}/>
-              <p>{ans.upVote.length - ans.downVote.length}</p>
-              <BiSolidDownvote onClick={()=>handleDownVote(ans._id)} />
+              <span onClick={()=>handleUpVote(ans._id, index, ans.upVote)}>
+                {currentUpvote[index]==1?<BiSolidUpvote />: <BiUpvote/>}
+              </span>
+              <p>{currentVotes[index]}</p>
+              <span onClick={()=>handleDownVote(ans._id, index, ans.downVote)}>
+                {currentDownvote[index]==1?<BiSolidDownvote/>: <BiDownvote/>}
+              </span>
+              
             </div>
             <div className="display-ans" key={ans?._id}>
               <p>{ans.answerBody}</p>
