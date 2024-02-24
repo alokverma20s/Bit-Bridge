ab427d537c9a6aa80cb40bfa77fadf2d1ad29602
Alok Verma <alokverma73762@gmail.com>
2024-02-24 12:21:42 +0530

    .

--- server/models/Department.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const DepartmentSchema = mongoose.Schema({
-    departmentName: {type: String, required: true},
-    subjects: [{type: mongoose.Schema.Types.ObjectId, ref: "Subject"}],
-    
-})
-
-export default mongoose.model("Department", DepartmentSchema);
