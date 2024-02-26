bd50c5d702bda088d9f2cbffcfc6a6a8fd5e3b02
Alok Verma <alokverma73762@gmail.com>
2024-02-26 10:39:16 +0530

    completing home page

--- client/src/Pages/Home/Hero.jsx
+++ client/src/Pages/Home/Hero.jsx
@@ -5,6 +5,7 @@ import ganeshji from "../../assets/ganeshji.png"
 import { NavLink } from "react-router-dom";
 import macbookImg from '../../assets/pngimg.com - macbook_PNG101760_out.png'
 import quizImg from '../../assets/quiz-animation.gif'
+import { IoMdMail } from "react-icons/io";
 
 const Hero = () => {
   const [typeEffect] = useTypewriter({
@@ -90,9 +91,52 @@ const Hero = () => {
         <div className="contentThree-img">
           <img src={quizImg} alt="" />
         </div>
+        <div className="contentThree-heading">
+          <h1>Your ultimate tool for mastering college subjects!</h1>
+          <h1>Elevate your learning experience by diving into tailored quizzes for each subject, designed to reinforce your understanding and boost your confidence.</h1>
+        </div>
       </div>
 
       <div className="footer">
+        <div className="footer-left">
+          <div className="footer-logo">
+            <h1><span>BIT</span> <span>BRIDGE</span></h1>
+            <p>The Ultimate Guide To Ace SDE Interviews.</p>
+          </div>
+          <div className="footer-links">
+
+            <h5 className="footer-heading">Quick Links</h5>
+          
+            <NavLink to='/Questions' className='footer-btn' activeClass='active'>
+              <p>Questions</p>
+            </NavLink>
+            <NavLink to='/Tags' className='footer-btn' activeClass='active'>
+              <p>Tags</p>
+            </NavLink>
+            <NavLink to='/Subjects' className='footer-btn' activeClass='active'>
+              <p>Subjects</p>
+            </NavLink>
+            {/* <NavLink to='/Users' className='footer-btn' activeClass='active'>
+              <p>Users</p>
+            </NavLink> */}
+            <NavLink to='/Quiz' className='footer-btn' activeClass='active'>
+              <p>Quiz</p>
+            </NavLink>
+          </div>
+        </div>
+        <div className="footer-right">
+          <div className="footer-developer">
+          <h5 className="footer-heading">Developers</h5>
+          <p className="footer-sub-heading">Alok Verma</p>
+          <p className="footer-sub-heading">Abhay Khator</p>
+          <p className="footer-sub-heading">Gaurav Ghidode</p>
+          <p className="footer-sub-heading">Iris Sahu</p>
+          </div>
+          <div className="footer-touch">
+            <h5 className="get-touch">GET IN TOUCH</h5>
+            <a className="footer-mail" href="mailto:bitbridge4@gmail.com"><span className="mail-icon"><IoMdMail /></span> <p>bitbridge4@gmail.com</p></a>
+          </div>
+        </div>
       </div>
     </>
   );
