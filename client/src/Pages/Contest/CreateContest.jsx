87489b321c97e03c65a572220ea65a97e48d8d01
Alok Verma <alokverma73762@gmail.com>
2024-04-20 09:52:29 +0530

    Adjusting time according to india

--- client/src/Pages/Contest/CreateContest.jsx
+++ client/src/Pages/Contest/CreateContest.jsx
@@ -37,33 +37,15 @@ const CreateContest = () => {
   const handleStartTimeChange = (event) => {
     const inputTime = event.target.value;
     const userDateTime = new Date(inputTime);
-    
-    // // Add 5 hours and 30 minutes to the user input
-    // console.log(inputTime);
-    // console.log(userDateTime)
-    // console.log(userDateTime.toISOString());
-    // userDateTime.setHours(userDateTime.getHours() + 5);
-    // userDateTime.setMinutes(userDateTime.getMinutes() + 30);
-    // console.log(userDateTime);
-    // console.log(userDateTime.toISOString());
-
-    // Update stored time with the adjusted value
     setContestData({...contestData ,startTime:userDateTime.toISOString()});
   };
 
   const handleEndTimeChange = (event) => {
     const inputTime = event.target.value;
     const userDateTime = new Date(inputTime);
-    
-    // Add 5 hours and 30 minutes to the user input
-    // userDateTime.setHours(userDateTime.getHours() + 5);
-    // userDateTime.setMinutes(userDateTime.getMinutes() + 30);
-
-    // Update stored time with the adjusted value
     setContestData({...contestData ,endTime:userDateTime.toISOString()});
   };
 
-  // console.log(contestData);
   const [loading, setLoading] = useState(false);
   const [problems, setProblems] = useState([]);
 
@@ -111,10 +93,10 @@ const CreateContest = () => {
                 onChange={(e) => {
                   setContestData({ ...contestData, name: e.target.value });
                 }}
-                className="h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal outline outline-0 transition-all placeholder-shown:border focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0"
                 placeholder=" "
               />
-              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300">
                 Contest's Name
               </label>
             </div>
@@ -129,10 +111,10 @@ const CreateContest = () => {
                     description: e.target.value,
                   });
                 }}
-                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal outline outline-0 transition-all placeholder-shown:border focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0"
                 placeholder=" "
               />
-              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300">
                 Description
               </label>
             </div>
@@ -147,10 +129,10 @@ const CreateContest = () => {
                     duration: e.target.value,
                   });
                 }}
-                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal outline outline-0 transition-all placeholder-shown:border focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0"
                 placeholder=" "
               />
-              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300">
                 Duration in minutes
               </label>
             </div>
@@ -162,10 +144,10 @@ const CreateContest = () => {
                 onChange={(e) => {
                   handleStartTimeChange(e);
                 }}
-                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal outline outline-0 transition-all placeholder-shown:border focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0"
                 placeholder=" "
               />
-              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300">
                 Start Time
               </label>
             </div>
@@ -177,10 +159,10 @@ const CreateContest = () => {
                 onChange={(e) => {
                   handleEndTimeChange(e);
                 }}
-                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                className="peer h-full w-full rounded-md border border-blue-gray-200 bg-white px-3 py-3 font-sans text-sm font-normal outline outline-0 transition-all placeholder-shown:border focus:border-2 focus:border-primary-300 focus:border-t-transparent focus:outline-0"
                 placeholder=" "
               />
-              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-primary-300 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-primary-300 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-primary-300">
                 End Time
               </label>
             </div>
