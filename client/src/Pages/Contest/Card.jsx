2fb0499bce59b2384dab090c00322b7f1f91ce33
Gaurav <gauravghidode@gmail.com>
2024-05-03 11:22:49 +0530

    .

--- client/src/Pages/Contest/Card.jsx
+++ client/src/Pages/Contest/Card.jsx
@@ -98,20 +98,6 @@ const Card = ({ contest }) => {
           </button>
           <button className="flex items-center justify-center gap-2 rounded-full border border-white/50 px-5 py-3 text-lg font-medium text-white bg-primary-400 hover:bg-primary-500">
             <span onClick={handleOpen}>Instruction</span>
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-              >
-                <path
-                  d="M6.00156 13.4016L4.60156 12.0016L8.60156 8.00156L4.60156 4.00156L6.00156 2.60156L11.4016 8.00156L6.00156 13.4016Z"
-                  fill="white"
-                />
-              </svg>
-            </span>
             <IoIosArrowForward />
           </button>
         </div>
