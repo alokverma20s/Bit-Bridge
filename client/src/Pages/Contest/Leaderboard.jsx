ebbe7b3aca1f246b7c8247db5c80e04129daee85
Alok Verma <alokverma73762@gmail.com>
2024-06-23 14:04:26 +0100

    Handle missing data

--- client/src/Pages/Contest/Leaderboard.jsx
+++ client/src/Pages/Contest/Leaderboard.jsx
@@ -13,14 +13,14 @@ const Leaderboard = () => {
     const [currentUserData, setCurrentUserData] = useState({})
     const currentUser = useSelector((state) => state.currentUserReducer);
     
-    
+    console.log(leaderboard);
     
     useEffect(() => {
         dispatch(getLeaderboard(setLoading, setLeaderboard, contestId));
     }, []);
 
     useEffect(() => {
-        setCurrentUserData(leaderboard.find(user => user.user._id === currentUser?.result?._id));
+        setCurrentUserData(leaderboard.find(user => user?.user?._id === currentUser?.result?._id));
     }, [leaderboard, currentUserData]);
     return (
     <div className='mt-16'>
@@ -43,15 +43,15 @@ const Leaderboard = () => {
                         {currentUserData && 
                         <tr key={currentUserData?._id} className='border-b-primary-600 border-b mb-2'>
                             <td className='text-lg text-center font-semibold'>{currentUserData?.ranks}</td>
-                            <td className='text-lg text-center font-semibold'>{currentUserData?.user.name}</td>
-                            <td className='text-lg text-center font-semibold'>{currentUserData?.user.email}</td>
+                            <td className='text-lg text-center font-semibold'>{currentUserData?.user?.name}</td>
+                            <td className='text-lg text-center font-semibold'>{currentUserData?.user?.email}</td>
                             <td className='text-lg text-center font-semibold'>{currentUserData?.score}</td>
                             <td className='text-lg text-center font-semibold'>{new Date(currentUserData?.lastSubmission).toLocaleTimeString()}</td>
                         </tr>}
                             
                         {
                             leaderboard.map((user, index) => (
-                                <tr key={user._id} className='border-b-primary-600 border-b mb-2'>
+                                <tr key={user?._id ?? index} className='border-b-primary-600 border-b mb-2'>
                                     <td className='text-lg font-semibold text-center'>{user?.ranks}</td>
                                     <td className='text-lg font-semibold text-center'>{user?.user?.name}</td>
                                     <td className='text-lg font-semibold text-center'>{user?.user?.email}</td>
